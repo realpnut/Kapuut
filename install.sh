8ee4ec34ccba8d510bfa9534e7c15a5f4b17daaa
peanut <pnuthacking@gmail.com>
2026-05-16 22:25:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 set -e
+
 echo "[+] Creating virtual environment..."
 python3 -m venv venv
+
 echo "[+] Activating virtual environment..."
 source venv/bin/activate
+
 echo "[+] Upgrading pip..."
 pip install --upgrade pip
+
 echo "[+] Installing dependencies..."
-pip install -r requirements.txt
+pip install -e .
+
 echo "[+] Installation complete!"
-echo "[+] Activate the environment with: source venv/bin/activate"
+echo "--------------------------------------------------"
+echo "[+] To run the tool using Python, execute:"
+echo "    source venv/bin/activate && python3 src/kapuut/main.py"
+echo "--------------------------------------------------"
